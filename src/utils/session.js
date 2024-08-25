5ac6951fca26c10fdd73f44f2154bc94da83cd3e
Soham Chakraborty <schakraborty8455@gmail.com>
2024-08-25 19:11:23 +0530

    Fix: Sessions

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,7 +1,8 @@
 const session = require('express-session');
 const crypto = require('crypto');
+require('dotenv').config();
 
-const secretKey = crypto.randomBytes(32).toString('hex');
+const secretKey = process.env.SESSION_SECRET || '5086ece3c805708208394e285bd33250746b9ea7e3172f29a878da7d42f873e0';
 
 const sessionMiddleware = session({
     secret: secretKey,
