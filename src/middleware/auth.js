46af5b1c17a692b6280a8bd7f88049c2cf992b8e
Soham Chakraborty <schakraborty8455@gmail.com>
2024-08-26 02:46:16 +0530

    Fix: Authguard

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,12 @@
 module.exports = (req, res, next) => {
-    if (!req.session.user) {
-        return res.status(401).json({ message: 'Unauthorized' });
+    // if (!req.session.user) {
+    //     return res.status(401).json({ message: 'Unauthorized' });
+    // }
+    // next();
+    if (req.session && req.session.user) {
+        req.user = req.session.user; 
+        next();
+    } else {
+        res.status(401).json({ message: 'Unauthorized' });
     }
-    next();
 };
