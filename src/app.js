14c60bde3664288f90de110f60169a7edd8b33ab
Soham Chakraborty <schakraborty8455@gmail.com>
2024-09-01 22:03:58 +0530

    Refactor: CORS

--- src/app.js
+++ src/app.js
@@ -12,7 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(sessionMiddleware);
-app.use(cors({credentials: true}));
+app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+
+      ],
+      credentials: true,
+    })
+  );
 
 app.use('/api/exams', examRoutes);
 app.use('/api/question', questionRoutes);
