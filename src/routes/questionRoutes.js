fc6371745006fb18ad697be99f88db1c4f4124f3
Soham Chakraborty <schakraborty8455@gmail.com>
2024-11-18 20:56:51 +0530

    Refactor: exam route

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -6,6 +6,6 @@ const authMiddleware = require('../middleware/auth');
 router.post('/', authMiddleware, questionController.createQuestion);
 router.put('/:question_id', authMiddleware, questionController.updateQuestion);
 router.delete('/:question_id', authMiddleware, questionController.deleteQuestion);
-// router.delete('/set/:exam_id', authMiddleware, questionController.viewQuestion);
+router.get('/set/:exam_id', authMiddleware, questionController.viewQuestion);
 
 module.exports = router;
